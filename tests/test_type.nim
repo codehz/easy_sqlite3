6be702ff7b2007c633a8ff90f503109e63f17511
codehz <git@hertz.moe>
2023-01-27 21:57:31 +0800

    add test for iterator

--- tests/test_type.nim
+++ tests/test_type.nim
@@ -8,9 +8,16 @@ type
 proc createUsersTable() {.importdb: "create table users(id INTEGER PRIMARY KEY, username TEXT NOT NULL, email TEXT NOT NULL)".}
 proc insertUser(username, email: string) {.importdb: "insert into users(username, email) values ($username, $email)".}
 proc selectUserById(id: int): User {.importdb: "select * from users where id = $id".}
+iterator iterateUsers(): User {.importdb: "select * from users".} = discard
 
-test "type decl":
-  var db = initDatabase(":memory:")
-  db.createUsersTable()
-  db.insertUser("user", "user@example.com")
-  check db.selectUserById(1) == (id: 1, username: "user", email: "user@example.com")
+suite "type decl":
+  setup:
+    var db = initDatabase(":memory:")
+    db.createUsersTable()
+    db.insertUser("user", "user@example.com")
+    db.insertUser("abc", "abc@example.com")
+  test "typed query":
+    check db.selectUserById(1) == (id: 1, username: "user", email: "user@example.com")
+  test "typed iterator":
+    for id, username, email in db.iterateUsers():
+      check email == (username & "@example.com")
